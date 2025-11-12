f92b65a9ac99482346c8885f67bbc57ec984c04d
ZackTai1363 <tai.cheng.zheng@student.mmu.edu.my>
2025-11-12 22:55:44 +0800

    Update app title for clarity

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 New app")
+st.title("🎈 Theee New app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
